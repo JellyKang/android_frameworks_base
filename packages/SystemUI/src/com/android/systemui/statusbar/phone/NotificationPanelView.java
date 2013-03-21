4387edc781a63498b3b1e838b8b654859c7f39e5
Emmanuel Utomi <emmanuelutomi@gmail.com>
2013-03-21 16:04:07 -0500

    Remove auto_open_quicksettings to prepare for merge

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -126,10 +126,8 @@ public class NotificationPanelView extends PanelView {
                             Settings.System.getInt(getContext().getContentResolver(),
                                     Settings.System.QS_QUICK_PULLDOWN, 0) == 2) {
                         flip = true;
-                    } else if ((!mStatusBar.hasClearableNotifications()) && (Settings.System.getInt(getContext().getContentResolver(),
-               Settings.System.AUTO_OPEN_QUICKSETTINGS, 0) == 1) &&
-                            Settings.System.getInt(getContext().getContentResolver(),
-                                    Settings.System.QS_QUICK_PULLDOWN, 0) == 3) {
+                    } else if (!mStatusBar.hasClearableNotifications() && Settings.System.getInt(getContext().getContentResolver(),
+                        Settings.System.QS_QUICK_PULLDOWN, 0) == 3) {
                         flip = true;
                     }
                     break;
