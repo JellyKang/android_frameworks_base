652aeec141040e7ac875e6be72b3c6094132ad2e
slickrick <rsalinas1@mail.usf.edu>
2013-04-27 21:54:59 -0400

    Hidden Statusbar Pulldown (1/2)
    Frameworks part for making "Swiping on the top of full screen apps shows status bar for 10 seconds"
    an on/off option.
    
    Big thanks to blk_jack for helping me on this!
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2815,6 +2815,12 @@ public final class Settings {
         public static final String HIDE_STATUSBAR = "hide_statusbar";
 
         /**
+         * Whether or not hidden Statusbar can be pulled down
+         * @hide
+         */
+        public static final String HIDDEN_STATUSBAR_PULLDOWN = "hidden_statusbar_pulldown";
+
+	/**
          * whether which Ram Usage Bar mode is used on recent switcher
          * 0 = none, 1 = only app use, 2 = app and cache use, 3 = app, cache and system use
          * @hide
