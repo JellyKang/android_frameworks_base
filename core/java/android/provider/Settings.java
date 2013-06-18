0df942dede009854da4c1e840d455b3df4a377ad
Emmanuel Utomi <emmanuelutomi@gmail.com>
2013-06-18 11:58:58 -0500

    missing var

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3290,6 +3290,12 @@ public final class Settings {
         public static final String HALO_ACTIVE = "halo_active";
 
         /**
+         * HALO, should default to 0 (no, do not show)
+         * @hide
+         */
+        public static final String HALO_ENABLED = "halo_enabled";
+
+        /**
          * HALO reversed?, should default to 1 (yes, reverse)
          * @hide
          */
